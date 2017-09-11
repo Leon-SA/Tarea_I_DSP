6dd6dc8beab5673d96cb9883574748724a17126f
Leon-SA <sleon08@hotmail.es>
2017-09-11 15:33:18 -0600

    Listos Ejercicios 1, 2,3, 5, 6, 7.

--- ReproducirNota.m
+++ ReproducirNota.m
@@ -1,12 +1,12 @@
 % 'ReproducirNota(N)' es una funcion definida por el usuario mediante el fichero 'ReproducirNota.m'.
 %
-% Reproduce una señal senoidal de 640 Hz a la frecuencia de muestreo que se desee reproducir.
+% Reproduce una señal de audio senoidal de 640 Hz a la frecuencia de muestreo 'fs' que se desee reproducir.
 %
 % El parametro 'N' indica la cantidad de muestras por periodo de la señal con la que se desea reproducir.
 
 function ReproducirNota(N)
   f = 640; % Frecuencia de la señal en Hz.
-  fm = N*f; % (muestras/segundo) = (muestras/periodo) * periodo.
-  t = 0:1/fm:3; % Duracion de la señal de 3 segundos.
+  fs = N*f; % (muestras/segundo) = (muestras/periodo) * periodo.
+  t = 0:1/fs:3; % Duracion de la señal de 3 segundos.
   y = sin(2*pi*f*t);
-  sound(y, fm);
+  sound(y, fs);
