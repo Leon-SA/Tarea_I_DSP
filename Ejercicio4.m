077df4c63d80d412534c597f0158fbd30e3f0aa0
Camila Gómez <camilagm994@gmail.com>
2017-09-13 17:19:16 -0600

    Cambios en ejercicio 4

--- Ejercicio4.m
+++ Ejercicio4.m
@@ -3,21 +3,21 @@
 n = 0:1:200; % Muestras.
 
 % Frecuencias.
-f1 = 0.003;
-f2 = 2009/3000; %Primera frecuencia alias de la fundamental f1
+w1 = 3*pi*0.003;
+w2 = w1 + 2*pi; %Primera frecuencia alias de la fundamental f1
 
 % Gr�ficas en mismo eje.
 
 figure(1);  % Crea Figura 1.
 grid; % Cuadr�cula.
 axis([0,200,-1.1,1.1]);
-title("Frecuencia  f = 0.003 vs su frecuencia alias f = 2009/3000")
+title("Frecuencia  w =3pi*0.003 vs su frecuencia alias w = 2.009pi")
 xlabel('n');
 ylabel('cos(3*pi*f*n)');
 hold on; % A�ade cambios sin borrar.
 
-stem(n, cos(3*pi*f1*n), 'g','filled'); % Grafica con frecuencia 1.
-stem(n, cos(3*pi*f2*n), 'b','filled'); % Grafica con frecuencia 2.
+stem(n, cos(w1*n), 'g','filled'); % Grafica con frecuencia 1.
+stem(n, cos(w2*n), 'b','filled'); % Grafica con frecuencia 2.
 
 hold off;
 
@@ -25,17 +25,17 @@ hold off;
 figure(2);  % Crea Figura 2.
 
 subplot(2,1,1); % Eje 1.
-stem(n, cos(3*pi*f1*n), 'g','filled'); % Grafica con frecuencia 1.
+stem(n, cos(w1*n), 'g','filled'); % Grafica con frecuencia 1.
 grid; % Cuadr�cula.
 axis([0,200,-1.1,1.1]);
-title("Frecuencia  f = 0.003")
+title("Frecuencia  w = 3pi*0.003")
 xlabel('n');
 ylabel('cos(3*pi*0.003*n)');
 
 subplot(2,1,2); % Eje 2.
-stem(n, cos(3*pi*f2*n), 'b','filled'); % Grafica con frecuencia 2.
+stem(n, cos(f2*n), 'b','filled'); % Grafica con frecuencia 2.
 grid; % Cuadr�cula.
 axis([0,200,-1.1,1.1]);
-title("Frecuencia alias f = 2009/3000")
+title("Frecuencia alias w = 3pi*0.003+2pi")
 xlabel('n');
-ylabel('cos(3*pi*2009/3000*n)');
+ylabel('cos(2.009pi*n)');
